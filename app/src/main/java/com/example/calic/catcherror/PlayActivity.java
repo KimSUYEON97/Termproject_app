3bf6c7d76646fa20cd14b5edfbf4c02293487842
KimSUYEON97 <calicitine97@naver.com>
2017-06-06 18:39:14 +0900

    화면터치시 좌표

--- app/src/main/java/com/example/calic/catcherror/PlayActivity.java
+++ app/src/main/java/com/example/calic/catcherror/PlayActivity.java
@@ -1,7 +1,11 @@
 package com.example.calic.catcherror;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -12,6 +16,8 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         gameView=new GameView(this);
         setContentView(gameView);
+        View view = new MyView(this);
+        setContentView(view);
         //setContentView(R.layout.activity_play);
     }
 
@@ -25,5 +31,25 @@ public class PlayActivity extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
         gameView.resume();
+    }
+
+    protected class MyView extends View {//화면 터치했을때 터치한 위치 좌표
+
+        public MyView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event){
+            super.onTouchEvent(event);
+            if(event.getAction()==MotionEvent.ACTION_DOWN){
+                float x= event.getX();
+                float y= event.getY();
+                String msg="get Touch: "+x+"/"+y;
+                Toast.makeText(PlayActivity.this,msg,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        }
     }
 }
